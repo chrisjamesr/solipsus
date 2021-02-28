a6669401c3d82f19bf76c0d3493a234ad8dbcd8b
Chris Rudolph <chrisjames.r@gmail.com>
2021-02-28 09:52:12 -0500

    generate controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  skip_before_action :authenticate
+  
+  def create
+    @user = User.new(user_params)
+    # if @user.save
+    #   jwt = Auth.issue({user: @user.id})
+    #   render json: {jwt: jwt, user_id: @user.id}
+    # else
+    #   render json: {errors: @user.errors.full_messages[0]}, status: 401
+    # end
+  end
+
+  def show
+    user = User.find(params[:id])
+    logged_in? ? (render json: user) : (head 403 )
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:id, :email, :password)
+    end
+
+  end 
+
+end
