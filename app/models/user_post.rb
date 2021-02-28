283832f019d86792b72e9387696bd8bb4fd222bd
Chris Rudolph <chrisjames.r@gmail.com>
2021-02-28 09:39:58 -0500

    add like method for future

--- app/models/user_post.rb
+++ app/models/user_post.rb
@@ -1,2 +1,11 @@
 class UserPost < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  # validates :user_id, :post_id, :presence => true
+  # validates :user_id, :uniqueness => {:scope => :event_id}
+
+  # def toggle_like
+  #   self.update(like: !self.like)
+  # end
 end
