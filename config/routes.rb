466d14ad8ac47c4e681918013ff6452cb913af4e
Chris Rudolph <chrisjames.r@gmail.com>
2021-02-28 10:56:52 -0500

    add resource routes for users & posts, add root route and session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#home'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html\
+
+  # users, user/posts, user/post
+  resources :users do
+    resources :posts
+  end
+
+  # get '/login' => 'sessions#new'
+  # post '/login' => 'sessions#create'
+  # delete '/logout' => 'sessions#destroy'
+
+  # post 'user_token' => 'user_token#create'
+  # resources :post_like, :as => :like, :path => :like, :only => [:create, :index, :update]
+  # get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  #   !request.xhr? && request.format.html?
+  # end
 end
