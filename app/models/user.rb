48f2db7afcba60dc7eed4c21e69efdf93b8c2739
Chris Rudolph <chrisjames.r@gmail.com>
2021-02-28 09:38:51 -0500

    paste auth methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  has_many :user_posts
-  has_many :posts, :through => user_posts
+  # has_many :user_posts
+  # has_many :posts, :through => user_posts
+  has_many :posts
 
   has_secure_password
 
@@ -10,5 +11,21 @@ class User < ApplicationRecord
   validates :email, uniqueness: {case_sensitive: false}
   validates :password, presence: true, allow_blank: false, on: :create
   validates_format_of :email, with: /@/
+
+
+  # def self.from_token_payload(payload)
+  #   self.find payload["sub"]
+  # end
+
+  # def to_token_payload
+  #       {
+  #           sub: id,
+  #           email: email
+  #       }
+  #   end
+
+  # def username
+  #   @username = self.email.match(/.+(?=@)/)[0]    
+  # end 
 
 end
